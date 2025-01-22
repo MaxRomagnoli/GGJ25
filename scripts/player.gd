c7b07749370e44710326ad0caba94090558bf69e
MaxRomagnoli <info@maxromagnoli.com>
2025-01-22 13:14:31 +0100

    Improved player controller and camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends RigidBody3D
 
 var is_attacking := false
-@export var idle_velocity := 0.01
-@export var attacking_velocity := 1.00
+@export var idle_velocity := 1
+@export var attacking_velocity := 2
 @export var rotation_velocity := 5.0
 
 @onready var player: RigidBody3D = $"."
@@ -36,11 +36,19 @@ func _physics_process(delta: float) -> void:
 	#var input := zanzara.global_position
 	#input.x = Input.get_axis("move_right", "move_left")
 	#input.y = Input.get_axis("move_down", "move_up")
-	#if is_attacking:
-		#input.z = attacking_velocity
-	#else:
-		#input.z = idle_velocity
-	#apply_central_force(input * 1200.0 * delta)
+	var velocity = 1200.0 * delta
+	if is_attacking:
+		velocity *= attacking_velocity
+	else:
+		velocity *= idle_velocity
+	
+	#var forward: Vector3 = self.transform.looking_at() #get_global_transform().basis #Vector3.BACK
+	var forward: Vector3 = Vector3.BACK
+	self.apply_force(forward * velocity, Vector3.ZERO)
+	#var move = global_transform.basis * Vector3(0, 0, -velocity)
+	#var move = global_transform.origin * velocity
+	#move_and_collide(move * 1200.0 * delta)
+	#apply_central_force(velocity * 1200.0 * delta)
 	
 	# Rotate horizontally
 	var y_rotation = Input.get_axis("move_right", "move_left")
