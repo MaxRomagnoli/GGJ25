5ca60286a20c1593945af30f01a7b4d18bcdef69
MaxRomagnoli <info@maxromagnoli.com>
2025-01-22 16:00:01 +0100

    simple movement for player.gd and associate animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -23,44 +23,46 @@ func _input(event) -> void:
 func attack(attack: bool, vibrate: bool) -> void:
 	if attack:
 		is_attacking = true
-		animation_player.play("Mosquito_Attack")
 		if vibrate:
 			Input.start_joy_vibration(0, 0.5, 0, 0.2)
 	else:
 		is_attacking = false
-		animation_player.play("Mosquito_Idle")
 		if vibrate:
 			Input.stop_joy_vibration(0)
 	
 func _physics_process(delta: float) -> void:
-	#var input := zanzara.global_position
-	#input.x = Input.get_axis("move_right", "move_left")
-	#input.y = Input.get_axis("move_down", "move_up")
+	
 	var velocity = 1200.0 * delta
 	if is_attacking:
 		velocity *= attacking_velocity
 	else:
 		velocity *= idle_velocity
 	
-	#var forward: Vector3 = self.transform.looking_at() #get_global_transform().basis #Vector3.BACK
-	var forward: Vector3 = Vector3.BACK
-	self.apply_force(forward * velocity, Vector3.ZERO)
-	#var move = global_transform.basis * Vector3(0, 0, -velocity)
-	#var move = global_transform.origin * velocity
-	#move_and_collide(move * 1200.0 * delta)
-	#apply_central_force(velocity * 1200.0 * delta)
-	
 	# Rotate horizontally
 	var y_rotation = Input.get_axis("move_right", "move_left")
 	rotate_y(y_rotation * rotation_velocity * delta)
 	#main_camera.rotation_degrees.y = 180 + player.rotation_degrees.y
-	if y_rotation > 0:
-		animation_player.play("Mosquito_FlyRight_Loop")
-	elif y_rotation < 0:
-		animation_player.play("Mosquito_FlyLeft_Loop")
 	
 	# Rotate vertically
 	var x_rotation = Input.get_axis("move_down", "move_up")
-	if (zanzara.rotation_degrees.x > -30 and x_rotation < 0) or (zanzara.rotation_degrees.x < 30 and x_rotation > 0):
-		zanzara.rotate_x(x_rotation * rotation_velocity * delta)
-		zanzara.rotation_degrees.x = clamp(zanzara.rotation_degrees.x, -30, 30)
+	#if (zanzara.rotation_degrees.x > -30 and x_rotation < 0) or (zanzara.rotation_degrees.x < 30 and x_rotation > 0):
+		#zanzara.rotate_x(x_rotation * rotation_velocity * delta)
+		#zanzara.rotation_degrees.x = clamp(, -30, 30)
+	
+	# zanzara.rotation_degrees.x al posto di x_rotation
+	var forward: Vector3 = self.global_transform.basis * Vector3(0, x_rotation * 1200.0 * delta, velocity)
+	self.apply_force(forward, Vector3.ZERO)
+	
+	# Animation
+	if is_attacking:
+		animation_player.play("Mosquito_Attack_Loop")
+	elif y_rotation > 0:
+		animation_player.play("Mosquito_FlyRight_Loop")
+	elif y_rotation < 0:
+		animation_player.play("Mosquito_FlyLeft_Loop")
+	elif x_rotation > 0:
+		animation_player.play("Mosquito_Backward")
+	elif x_rotation < 0:
+		animation_player.play("Mosquito_Forward")
+	else:
+		animation_player.play("Mosquito_Idle")
